de342525f172467d077833ba01b1d49eb9ee1c20
belleqss <bellbi123@gmail.com>
2023-08-12 15:31:26 +0700

    add new file

---
+++ memory.c
@@ -0,0 +1,13 @@
+/*
+    Task	: memory
+    Author	: Belle
+    School	: Kasetsart University
+    Language	: C
+    Created	: 12 August 2023 [15:30]
+    Algo	: 
+    Status	: 
+*/
+#include<stdio.h>
+int main(){
+    return 0;
+}
